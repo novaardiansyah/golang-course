b512c77b508be7c3905b2e8df76773f0958b7b2c
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 02:23:06 +0700

    variable and default value

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var myRune rune = 'a'
+	fmt.Println(myRune)
+
+	var myBoolean bool = true
+	fmt.Println(myBoolean)
+
+	var intNum3 rune
+	fmt.Println(intNum3)
+
+	var myVar string = "Hello World"
+	fmt.Println(myVar)
+
+	var1, var2 := 1, 2
+	fmt.Println(var1, var2)
+
+	const myConst string = "Hello World"
+	fmt.Println(myConst)
+
+	const pi float64 = 3.14159
+	fmt.Println(pi)
 }
