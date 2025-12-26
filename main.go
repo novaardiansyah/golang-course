56a05749295f3102764e64943c4d4fcbde983f19
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 02:33:58 +0700

    Error validation

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	printMe("Hello World")
 
 	var numerator int8 = 10
-	var denominator int8 = 2
+	var denominator int8 = 0
 
-	var result, remainder int8 = intDevision(numerator, denominator)
+	var result, remainder, err = intDevision(numerator, denominator)
+	
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 	fmt.Printf("Result: %v, Remainder: %v\n", result, remainder)
 }
 
@@ -16,7 +25,12 @@ func printMe(printValue string) {
 	fmt.Println(printValue)
 }
 
-func intDevision(numerator int8, denominator int8) (int8, int8) {
-	return numerator / denominator, numerator % denominator
+func intDevision(numerator int8, denominator int8) (int8, int8, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("denominator cannot be zero")
+		return 0, 0, err
+	}
+	return numerator / denominator, numerator % denominator, nil
 }
 
