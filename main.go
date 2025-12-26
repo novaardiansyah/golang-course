5d6a0d2291605d8901712ac0ec3266b33eb5f1af
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 02:39:04 +0700

    array

--- main.go
+++ main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
-	printMe("Hello World")
+	var intArr [3]int32
 
-	var numerator int8 = 10
-	var denominator int8 = 2
+	intArr[0] = 1
+	intArr[1] = 2
+	intArr[2] = 3
 
-	var result, remainder, err = intDevision(numerator, denominator)
-	
-	switch {
-		case err != nil:
-			fmt.Println(err.Error())
-		case remainder == 0:
-			fmt.Printf("Result: %v", result)
-		default:
-			fmt.Printf("Result: %v, Remainder: %v\n", result, remainder)
-		}
+	fmt.Println(intArr)
 }
-
-func printMe(printValue string) {
-	fmt.Println(printValue)
-}
-
-func intDevision(numerator int8, denominator int8) (int8, int8, error) {
-	var err error
-	if denominator == 0 {
-		err = errors.New("denominator cannot be zero")
-		return 0, 0, err
-	}
-	return numerator / denominator, numerator % denominator, nil
-}
-
