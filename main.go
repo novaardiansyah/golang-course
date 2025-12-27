cd179e0cae20376840f3e0b60cadf5e8e03d35c5
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 10:28:31 +0700

    Replace engine struct and interface examples with a basic pointer demonstration.

--- main.go
+++ main.go
@@ -4,56 +4,13 @@ import (
 	"fmt"
 )
 
-type gasEngine struct {
-	mpg uint8
-	gallons uint8
-	ownerInfo owner
-}
-
-type owner struct {
-	name string
-	age uint8
-}
-
-
-func (e gasEngine) milesLeft() uint8 {
-	return e.mpg * e.gallons
-}
-
-type electricEngine struct {
-	mpkwh uint8
-	kwh uint8
-}
-
-func (e electricEngine) milesLeft() uint8 {
-	return e.mpkwh * e.kwh
-}
-
-type engine interface {
-	milesLeft() uint8
-}
-
-func canMakeIt(e engine, miles uint8) {
-	if miles <= e.milesLeft() {
-		fmt.Println("You can make it!")
-	} else {
-		fmt.Println("You can't make it!")
-	}
-}
-
 func main() {
-	var myEngine gasEngine = gasEngine{20, 10, owner{"John", 30}}
-	fmt.Println(myEngine)
+	var p *int = new(int)
+	var i int = 10
 
-	var myEngine2 = struct {
-		mpg uint8
-		gallons uint8
-	}{20, 10}
-	
-	fmt.Println(myEngine2)
+	fmt.Println(p)
+	fmt.Println(i)
 
-	fmt.Println(myEngine.milesLeft())
-
-	canMakeIt(myEngine, myEngine.milesLeft())
-	canMakeIt(electricEngine{20, 10}, 100)
+	fmt.Printf("\nThe value p points to is: %v\n", *p)
+	fmt.Printf("The value of i is: %v\n", i)
 }
