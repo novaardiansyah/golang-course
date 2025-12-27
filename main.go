bbe6675889ddd6c12a5a9ce4243355809c697ccd
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 10:07:27 +0700

    feat: explicitly declare string type and print string length

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 )
 
 func main() {
-	var myString = "Hello World"
+	var myString string = "Hello World"
 	var indexed = myString[0]
 	fmt.Printf("%v, %T\n", indexed, indexed)
 
 	for i, v := range myString {
 		fmt.Println(i, v)
 	}
+
+	fmt.Printf("\nThe length of 'myString' is %v bytes\n", len(myString))
 }
