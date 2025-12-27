3e8bb003827c9d3f1aeb4717d6f0c4ea098fad8f
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 10:01:36 +0700

    feat: replace slice performance benchmark with string indexing and iteration example

--- main.go
+++ main.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	var n uint32 = 10000000
-	var testSlice = []int32{}
-	var testSlice2 = make([]int32, 0, n)
+	var myString = "Hello World"
+	var indexed = myString[0]
+	fmt.Printf("%v, %T\n", indexed, indexed)
 
-	fmt.Printf("Total time without pre-allocation: %v\n", timeLoop(testSlice, n))
-	fmt.Printf("Total time with pre-allocation: %v\n", timeLoop(testSlice2, n))
-}
-
-func timeLoop(slice []int32, n uint32) time.Duration {
-	var t0 = time.Now()
-	for len(slice) < int(n) {
-		slice = append(slice, 1)
+	for i, v := range myString {
+		fmt.Println(i, v)
 	}
-	return time.Since(t0)
 }
