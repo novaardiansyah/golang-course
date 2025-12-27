4ae4eca40a766ca1557c3f491e1a6e29adb02980
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 09:50:03 +0700

    feat: add new entries to map initialization, implement map deletion, and demonstrate map iteration

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	var myMap map[string]uint8 = make(map[string]uint8)
 	fmt.Println(myMap)
 
-	var myMap2 = map[string]uint8{"Adam": 25, "Eve": 22}
+	var myMap2 = map[string]uint8{"Adam": 25, "Eve": 22, "Jane": 24, "Lucy": 23}
 	fmt.Println(myMap2)
 
 	fmt.Println(myMap2["John"])
@@ -20,5 +20,16 @@ func main() {
 		fmt.Println("John is", age)
 	} else {
 		fmt.Println("John is not in the map")
+	}
+
+	delete(myMap2, "Adam")
+	fmt.Println(myMap2)
+
+	for name := range myMap2 {
+		fmt.Printf("Name: %v, Age: %v\n", name, myMap2[name])
+	}
+
+	for i, v := range myMap2 {
+		fmt.Printf("Key: %v, Value: %v\n", i, v)
 	}
 }
