569a1774138473bf7aa26e4ab0a52c968f4dee5c
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 10:12:33 +0700

    refactor: Replace manual string concatenation with `strings.Builder` and remove unused rune slice examples.

--- main.go
+++ main.go
@@ -2,28 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	var myString = []rune("Hello World")
-	var indexed = myString[0]
-	fmt.Printf("%v, %T\n", indexed, indexed)
-
-	for i, v := range myString {
-		fmt.Println(i, v)
-	}
-
-	fmt.Printf("\nThe length of 'myString' is %v bytes\n", len(myString))
-
-	var myRune = 'a'
-	fmt.Printf("%v, %T\n", myRune, myRune)
-
 	var strSlice = []string{"s", "u", "n", "d", "a", "y"}
-	var catStr = ""
+	var strBuilder strings.Builder
 
 	for i := range strSlice {
-		catStr += strSlice[i]
+		strBuilder.WriteString(strSlice[i])
 	}
-
+	
+	var catStr = strBuilder.String()
 	fmt.Printf("%v, %T\n", catStr, catStr)
 }
