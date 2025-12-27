5662b63a20baabbbda6a26b847053f7987a874b9
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 09:35:46 +0700

    feat: Add Go slice declaration, initialization, and append example

--- main.go
+++ main.go
@@ -8,4 +8,10 @@ func main() {
 	// var intArr [3]int32 = [3]int32{1, 2, 3}
 	intArr := [...]int32{1, 2, 3}
 	fmt.Println(intArr)
+
+	var intSlice []int32 = []int32{4, 5, 6}
+	fmt.Println(intSlice)
+
+	intSlice = append(intSlice, 7)
+	fmt.Println(intSlice)
 }
