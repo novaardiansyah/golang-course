c64bf5a1c234659791d6589b56d40ff73a8f6b9d
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 10:19:16 +0700

    feat: Add `milesLeft` method to `gasEngine` and demonstrate anonymous struct initialization.

--- main.go
+++ main.go
@@ -15,7 +15,21 @@ type owner struct {
 	age uint8
 }
 
+
+func (e gasEngine) milesLeft() uint8 {
+	return e.mpg * e.gallons
+}
+
 func main() {
 	var myEngine gasEngine = gasEngine{20, 10, owner{"John", 30}}
 	fmt.Println(myEngine)
+
+	var myEngine2 = struct {
+		mpg uint8
+		gallons uint8
+	}{20, 10}
+	
+	fmt.Println(myEngine2)
+
+	fmt.Println(myEngine.milesLeft())
 }
