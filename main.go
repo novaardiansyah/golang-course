70c01993a35d29132774044ababcb15e6edf6755
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 09:58:02 +0700

    feat: compare slice append performance with and without pre-allocation

--- main.go
+++ main.go
@@ -2,34 +2,22 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	var myMap map[string]uint8 = make(map[string]uint8)
-	fmt.Println(myMap)
+	var n uint32 = 10000000
+	var testSlice = []int32{}
+	var testSlice2 = make([]int32, 0, n)
 
-	var myMap2 = map[string]uint8{"Adam": 25, "Eve": 22, "Jane": 24, "Lucy": 23}
-	fmt.Println(myMap2)
+	fmt.Printf("Total time without pre-allocation: %v\n", timeLoop(testSlice, n))
+	fmt.Printf("Total time with pre-allocation: %v\n", timeLoop(testSlice2, n))
+}
 
-	fmt.Println(myMap2["John"])
-	
-	var age, ok = myMap2["John"]
-	fmt.Println(age, ok)
-
-	if ok {
-		fmt.Println("John is", age)
-	} else {
-		fmt.Println("John is not in the map")
+func timeLoop(slice []int32, n uint32) time.Duration {
+	var t0 = time.Now()
+	for len(slice) < int(n) {
+		slice = append(slice, 1)
 	}
-
-	delete(myMap2, "Adam")
-	fmt.Println(myMap2)
-
-	for name := range myMap2 {
-		fmt.Printf("Name: %v, Age: %v\n", name, myMap2[name])
-	}
-
-	for i, v := range myMap2 {
-		fmt.Printf("Key: %v, Value: %v\n", i, v)
-	}
+	return time.Since(t0)
 }
