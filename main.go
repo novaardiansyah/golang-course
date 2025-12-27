df3e140b9950d2b0ea7b2d51ce01b87bbf82b4c1
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 10:15:15 +0700

    feat: Add owner struct and ownerInfo field to gasEngine, initializing it with owner data.

--- main.go
+++ main.go
@@ -7,9 +7,15 @@ import (
 type gasEngine struct {
 	mpg uint8
 	gallons uint8
+	ownerInfo owner
+}
+
+type owner struct {
+	name string
+	age uint8
 }
 
 func main() {
-	var myEngine gasEngine = gasEngine{mpg: 20, gallons: 10}
+	var myEngine gasEngine = gasEngine{20, 10, owner{"John", 30}}
 	fmt.Println(myEngine)
 }
